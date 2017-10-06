996940c3643e9af84834dd2fc9f524eaba7fb314
Alex Smith <alex_smith3@hotmail.com>
2017-10-06 13:47:01 +0100

    completed film class

--- console.rb
+++ console.rb
@@ -1,8 +1,10 @@
 require_relative('models/customers')
+require_relative('models/films')
 
 require('pry-byebug')
 
 Customer.delete_all()
+Film.delete_all()
 
 
 customer1 = Customer.new({
@@ -11,7 +13,7 @@ customer1 = Customer.new({
   })
 customer2 = Customer.new({
   'name' => 'Bob',
-  'funds' => '30'
+  'funds' => 30
   })
 customer3 = Customer.new({
   'name' => 'Carol',
@@ -21,6 +23,24 @@ customer3 = Customer.new({
 customer1.save()
 customer2.save()
 customer3.save()
+
+film1 = Film.new({
+  'title' => 'Age of Ultron',
+  'price' => 10
+  })
+film2 = Film.new({
+  'title' => 'Bladerunner',
+  'price' => 10
+  })
+film3 = Film.new({
+  'title' => 'Colossus: The Forbin Project',
+  'price' => 10
+  })
+
+film1.save()
+film2.save()
+film3.save()
+
 
 binding.pry
 nil
---
+++ models/films.rb
@@ -0,0 +1,90 @@
+require_relative("../db/sql_runner")
+
+class Film
+
+  attr_reader :id
+  attr_accessor :title, :price
+
+  def initialize (details)
+    @id = details['id'].to_i
+    @title = details['title']
+    @price = details['price'].to_i
+  end
+
+#create
+  def save()
+    sql = "INSERT INTO films
+    (
+      title,
+      price
+    )
+    VALUES
+    (
+      $1, $2
+    )
+    RETURNING id;"
+    values = [@title, @price]
+    film = SqlRunner.run( sql, values ).first
+    @id = film['id'].to_i
+  end
+
+#read
+
+  def self.all()
+    sql = "SELECT * FROM films;"
+    values = []
+    films = SqlRunner.run(sql, values)
+    result = films.map { |film| Film.new(film) }
+    return result
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM films WHERE id = $1;"
+    values = [id]
+    results = SqlRunner.run(sql, values)
+    film = results.first[0]
+    return Film.new(film)
+  end
+
+#update
+
+  def update()
+   sql = "
+   UPDATE films SET (
+     title,
+     price
+   ) =
+   (
+     $1, $2
+   )
+   WHERE id = $3;"
+   values = [@title, @price, @id]
+   SqlRunner.run(sql, values)
+ end
+
+#delete
+
+ def self.delete_all()
+   sql = "DELETE FROM films;"
+   values = []
+   SqlRunner.run(sql, values)
+ end
+
+ def delete()
+   sql = "DELETE FROM films where id = $1;"
+   values = [@id]
+   SqlRunner.run(sql, values)
+ end
+
+#  def customer()
+#   sql = "SELECT customers.* FROM customers
+#         INNER JOIN tickets
+#         ON tickets.customer_id = customer.id
+#         WHERE film_id = $1;"
+#   values = [@id]
+#   customers = SqlRunner.run(sql, values)
+#   return customers.map { |customer| Customer.new(customer) }
+# end
+
+
+end
